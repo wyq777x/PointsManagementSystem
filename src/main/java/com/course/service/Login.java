759b379228408fb8a11ff2e89a49c0dc742b53d6
Wyatt Wong <635118061@qq.com>
2026-06-22 08:54:27 +0800

    fix: 解决编译问题

--- src/main/java/com/course/service/Login.java
+++ src/main/java/com/course/service/Login.java
@@ -3,16 +3,20 @@ package com.course.service;
 import com.course.pojo.PointObject;
 import com.course.utils.FileUtils;
 import com.course.utils.JsonUtils;
+import org.springframework.stereotype.Component;
 
 /**
  * @author lixuy
  * Created on 2019-04-11
  */
-//类名与方法名须与controller层拦截的方法一致
-public class Login {
+// 类名与方法名须与controller层拦截的方法一致
+@Component
+// 登录平台
+public class Login
+{
 
-    public void testDesign(){
-        System.out.println("+++++login积分计算方法执行+++++");
+    public void login()
+    {
+        System.out.println("======被拦截的login方法执行======");
     }
-
 }
